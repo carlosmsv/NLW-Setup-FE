6c4635e99ef26c5b9df4b0530ab39640d7847b3a
Carlos <carlos.msv@hotmail.com>
2023-01-18 08:15:27 -0300

    button hover

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ export function App() {
 
           <button 
             type="button"
-            className='border border-violet-500 font-semibold rounded-lg px-6 py-4 flex items-center gap-3'
+            className='border border-violet-500 font-semibold rounded-lg px-6 py-4 flex items-center gap-3 hover:border-violet-300'
           >
             <Plus size={20} className="text-violet-500"/>
             Novo hábito

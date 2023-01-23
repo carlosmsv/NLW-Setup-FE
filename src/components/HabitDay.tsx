bc65efb86b02d6c6727ad51c1333ff2a1c26ba5a
Carlos <carlos.msv@hotmail.com>
2023-01-23 01:42:54 -0300

    fix: day and month displayed inside HabitDay component

--- src/components/HabitDay.tsx
+++ src/components/HabitDay.tsx
@@ -30,7 +30,7 @@ export function HabitDay({ completed = 0, amount = 0, date }: HabitDayProps){
       <Popover.Portal>
         <Popover.Content className="min-w-[320px] p-6 rounded-2xl bg-zinc-900 flex flex-col">
           <span className="font-semibold text-zinc-400">Terça-feira</span>
-          <span className="mt-1 font-extrabold leading-tight text-3xl">dayAndMonth</span>
+          <span className="mt-1 font-extrabold leading-tight text-3xl">{dayAndMonth}</span>
 
           <ProgressBar progress={completedPercentage} />
 

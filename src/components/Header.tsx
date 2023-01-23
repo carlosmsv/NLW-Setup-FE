2b1bfb6c04cbd9d846655a625b4160d8bcd3a061
Carlos <carlos.msv@hotmail.com>
2023-01-22 21:56:47 -0300

    using radix-ui dialog

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,30 +1,31 @@
 import { Plus } from 'phosphor-react'
-import { useState } from 'react'
+import * as Dialog from '@radix-ui/react-dialog'
 
 import logoImage from '../assets/logo.svg'
 
 export function Header(){
-  const [isModalOpen, setIsModalOpen] = useState(false)
-
-  function buttonClicked(){
-    setIsModalOpen(true)
-  }
-
   return (
     <div className ="w-full max-w-3xl mx-auto flex items-center justify-between">
       <img src={ logoImage } alt="Habits"/>
 
-      {isModalOpen ? <p>oi</p> : null}
+      <Dialog.Root>
+        <Dialog.Trigger
+          type="button"
+          className='border border-violet-500 font-semibold rounded-lg px-6 py-4 flex items-center gap-3 hover:border-violet-300'
+        >
+          <Plus size={20} className="text-violet-500"/>
+          Novo hábito
+        </Dialog.Trigger>
 
-      <button 
-        type="button"
-        onClick={buttonClicked}
-        className='border border-violet-500 font-semibold rounded-lg px-6 py-4 flex items-center gap-3 hover:border-violet-300'
-      >
-
-      <Plus size={20} className="text-violet-500"/>
-      Novo hábito
-      </button>
+        {/* Portal to place the modal content outside of Header, because it's supposed to be on the Body */}
+        <Dialog.Portal>
+          <Dialog.Overlay />
+          
+          <Dialog.Content>
+            Conteúdo do Modal
+          </Dialog.Content>
+        </Dialog.Portal>
+      </Dialog.Root> 
     </div>
   )
 }

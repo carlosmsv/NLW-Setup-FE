f8b0bb2e1ba32952293d1dafdff436b366e39a76
Carlos <carlos.msv@hotmail.com>
2023-01-22 22:54:23 -0300

    updated HabitDayProps

--- src/components/HabitDay.tsx
+++ src/components/HabitDay.tsx
@@ -1,10 +1,12 @@
 import * as Popover from '@radix-ui/react-popover'
 import { ProgressBar } from './ProgressBar'
-// interface HabitDayProps {
-//   completed: number
-// }
 
-export function HabitDay(props: any){
+interface HabitDayProps {
+  completed: number
+  amount: number
+}
+
+export function HabitDay(props: HabitDayProps){
   return (
     <Popover.Root>
       <Popover.Trigger className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg" />

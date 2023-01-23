2df80c3ba3021fb0bb93f8c7f99b9abb8abfd74e
Carlos <carlos.msv@hotmail.com>
2023-01-22 22:58:29 -0300

    HabitDay ProgressBar completed percentage

--- src/components/HabitDay.tsx
+++ src/components/HabitDay.tsx
@@ -7,6 +7,8 @@ interface HabitDayProps {
 }
 
 export function HabitDay({ completed, amount }: HabitDayProps){
+  const completedPercentage = Math.round((completed / amount) * 100)
+
   return (
     <Popover.Root>
       <Popover.Trigger className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg" />
@@ -16,7 +18,7 @@ export function HabitDay({ completed, amount }: HabitDayProps){
           <span className="font-semibold text-zinc-400">Terça-feira</span>
           <span className="mt-1 font-extrabold leading-tight text-3xl">17/01</span>
 
-          <ProgressBar progress={75} />
+          <ProgressBar progress={completedPercentage} />
 
           <Popover.Arrow height={8} width={16} className="fill-zinc-900"/>
         </Popover.Content>

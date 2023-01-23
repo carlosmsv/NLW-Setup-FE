ef0aa991e41e839a5ab16c7a4e6c1a630aa03aeb
Carlos <carlos.msv@hotmail.com>
2023-01-22 22:29:07 -0300

    HabitDay Popover

--- src/components/HabitDay.tsx
+++ src/components/HabitDay.tsx
@@ -1,9 +1,18 @@
+import * as Popover from '@radix-ui/react-popover'
 // interface HabitDayProps {
 //   completed: number
 // }
 
 export function HabitDay(props: any){
   return (
-    <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg"></div>
+    <Popover.Root>
+      <Popover.Trigger className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg" />
+
+      <Popover.Portal>
+        <Popover.Content className="min-w-[320px] p-6 rounded-2xl bg-zinc-900 flex flex-col">
+          Test
+        </Popover.Content>
+      </Popover.Portal>
+    </Popover.Root>
   )
 }

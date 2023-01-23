e2f6629fbe0fc5d198baaca5fe198e8afaf6c73d
Carlos <carlos.msv@hotmail.com>
2023-01-23 01:38:31 -0300

    completedPercentage only calculated if amount > 0

--- src/components/HabitDay.tsx
+++ src/components/HabitDay.tsx
@@ -11,7 +11,7 @@ interface HabitDayProps {
 }
 
 export function HabitDay({ completed = 0, amount = 0 }: HabitDayProps){
-  const completedPercentage = Math.round((completed / amount) * 100)
+  const completedPercentage = amount > 0 ? Math.round((completed / amount) * 100) : 0
 
   return (
     <Popover.Root>
